4d8c59340184ab18a95bfab751aba33ae049c278
Janos Gyerik <janos.gyerik+windows@sonarsource.com>
2017-10-10 12:56:34 +0200

    First version

---
+++ IssueBackdatingExample1/Employee.Night.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IssueBackdatingExample1
+{
+    partial class Employee
+    {
+        public void DoNightActivity()
+        {
+            Console.WriteLine("PARTY HARD !!!");
+        }
+    }
+}
---
+++ IssueBackdatingExample1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IssueBackdatingExample1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            employee.DoDayActivity();
+            employee.DoNightActivity();
+        }
+    }
+}
